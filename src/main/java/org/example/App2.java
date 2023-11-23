0ff3d6b943f94153b6f579d1848a05f2fa5630ff
asmae <asmaeelhyani.23@gmail.com>
2023-11-23 23:31:36 +0100

    final commit

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -8,16 +8,6 @@ public class App2 {
     public static void main(String[] args) {
         SparkSession ss=SparkSession.builder().appName("TP SPARK SQL").master("local[*]").getOrCreate();
         //Les noms des tables dans mon base de donnee : patients, 	medecins,consultations
-        Dataset<Row> df=ss.read().format("jdbc")
-                .option("driver","com.mysql.jdbc.Driver")
-                .option("url","jdbc:mysql://localhost:3306/mastersdia")
-                .option("user","root")
-                .option("dbtable","patients")
-                //.option("query","")
-                .option("password","")
-                .load();
-        //Afficher le schema
-        df.printSchema();
 
         //- Afficher le nombre de consultations par jour.
         Dataset<Row> df1=ss.read().format("jdbc")
@@ -25,12 +15,36 @@ public class App2 {
                 .option("url","jdbc:mysql://localhost:3306/mastersdia")
                 .option("user","root")
                 //.option("dbtable","patients")
-                .option("query","")
+                .option("query","SELECT `DATE_CONSULTATION`,COUNT(*) as consultations " +
+                        " FROM `consultations` GROUP BY `DATE_CONSULTATION`  ")
                 .option("password","")
                 .load();
+        df1.show();
         //- Afficher le nombre de consultation par médecin. Le format d’affichage est le suivant :
         //NOM | PRENOM | NOMBRE DE CONSULTATION
+        Dataset<Row> df2=ss.read().format("jdbc")
+                .option("driver","com.mysql.jdbc.Driver")
+                .option("url","jdbc:mysql://localhost:3306/mastersdia")
+                .option("user","root")
+                .option("query","select m.nom,m.prenom,count(*) as nombre_de_consultation " +
+                        "from consultations c JOIN medecins m ON c.ID_MEDECIN=m.ID " +
+                        "group by m.NOM,m.PRENOM")
+                .option("password","")
+                .load();
+        df2.show();
+
         //- Afficher pour chaque médecin, le nombre de patients qu’il a assisté.
+        Dataset<Row> df3=ss.read().format("jdbc")
+                .option("driver","com.mysql.jdbc.Driver")
+                .option("url","jdbc:mysql://localhost:3306/mastersdia")
+                .option("user","root")
+                .option("query","select m.nom as le_nom_de_médecin ,m.prenom as le_prénom_de_médecin," +
+                        "p.NOM  as le_nom_de_patient,p.PRENOM as le_prenom_de_patient " +
+                        "from consultations c JOIN medecins m ON c.ID_MEDECIN=m.ID " +
+                        "join patients p ON c.ID_PATIENT=p.ID ")
+                .option("password","")
+                .load();
+        df3.show();
     }
 }
 
